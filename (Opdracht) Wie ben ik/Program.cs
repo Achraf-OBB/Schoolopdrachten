ac29f03b9493a1eb4da484096d5e138e96890c2b
Achraf-OBB <ouladbenbrahima@gmail.com>
2025-09-30 12:27:42 +0200

    Add files via upload

---
+++ (Opdracht) Wie ben ik/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _Opdracht__Wie_ben_ik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Achraf Oulad Ben Brahim");
+            Console.WriteLine("Alkmaar");
+            Console.WriteLine("21");
+        }
+    }
+}
